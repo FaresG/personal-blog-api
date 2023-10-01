f6bdf40e2a35502c51e722c97f1e68b00a19f248
Fares Gdoura <gdourafares@gmail.com>
2023-10-01 12:37:00 +0100

    delete api tokens job

---
+++ app/Jobs/DeleteApiTokens.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+
+class DeleteApiTokens implements ShouldQueue, ShouldBeUnique
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        // Delete tokens if surpasses 30 days, to force refresh.
+        DB::table('personal_access_tokens')
+            ->where('created_at', '<', Carbon::now()->subDays(30)->toDateTimeString())
+            ->delete();
+    }
+}
